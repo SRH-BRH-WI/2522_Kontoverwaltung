90b39304e94e8dfdde21d6a8561f517a07ee8296
Dr. Dillinger <peter.dillinger@srh.de>
2023-05-16 12:01:11 +0200

    JUnit ergänzt, evtl. über ProjectStructure -> Libraries -> Maven -> junit (höchste Zahl) hinzufügen

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,7 +1,19 @@
 package controller;
 
+import model.Girokonto;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+    }
+
+    @Test
+    public void TestFürGirokonten() {
+        Girokonto giro = new Girokonto(1, "Mr. Giro", 500, 350);
+
+        Assert.assertEquals("Check der Kontonummer", 1, giro.getKontonummer() );
+        Assert.assertEquals("Check vom Inhaber", "Mr. Giro", giro.getInhaber());
     }
 }
